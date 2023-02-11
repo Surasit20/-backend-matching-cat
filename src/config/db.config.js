a853e684b37cb9ba747793f91a9528b96ae64617
Qryptoverse <qryptoverse@gmail.com>
2023-02-12 00:45:07 +0700

    fix address

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,15 +2,18 @@
   db: 'mongodb+srv://Surasit:O8X7hT9y1nrZOyRU@cluster0.u3exbej.mongodb.net/matching-cats?retryWrites=true&w=majority',
 };
 
-module.exports = {
-  db: 'mongodb+srv://cat:1234@cluster0.yr99reb.mongodb.net/matching-cats?retryWrites=true&w=majority',
-};
+
 
 */
 
 /*
 };
-*/
+
 module.exports = {
   db: 'mongodb://localhost:27017/matching-cats',
+};
+
+*/
+module.exports = {
+  db: 'mongodb+srv://cat:1234@cluster0.yr99reb.mongodb.net/matching-cats?retryWrites=true&w=majority',
 };
